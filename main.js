21e4c03071d1c558cf7af041088c743cc568d547
nishe66 <sumiyajahan29@gmail.com>
2024-10-06 20:18:47 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,81 @@
+// Math Number 1
+let SaraHasApple = 25;
+let BuyExtraApple = 15;
+
+let TotalApple = SaraHasApple + BuyExtraApple;
+console.log('Sara has total Apple: ' + TotalApple);
+// Sara has total Apple: 40
+
+// Math Number 2
+let JohnHadMoney = 500;
+let SpentMoney = 200;
+
+let TotalMoney = JohnHadMoney - SpentMoney;
+console.log('John has Total Money: ' + TotalMoney);
+// John has Total Money: 300
+
+// Math Number 3
+let FactoryMadeMachine = 120;
+let EightHourMachine = 8;
+
+let TotalMachine = FactoryMadeMachine * EightHourMachine;
+console.log('factory makes a total of machines in eight hours: ' +  TotalMachine);
+// factory makes a total of machines in eight hours: 960
+
+
+// Math Number 4
+let EveryRowHasTree = 12;
+let TotalRow = 8;
+let ExtraTreePlan = 20;
+
+let TotalTree = EveryRowHasTree * TotalRow + ExtraTreePlan;
+console.log('There will be total tree in garden :' + TotalTree);
+// There will be total tree in garden :116
+
+// Math Number 5
+let RahimHasBox = 5;
+let EveryBoxOrange = 10;
+let GaveOrange = 12;
+
+let TotalOrange = RahimHasBox * EveryBoxOrange - GaveOrange;
+console.log('Rahim has total orange: ' + TotalOrange);
+// Rahim has total orange: 38
+
+// Math Number 6
+let TrainCrossPlace = 150;
+let TakeTimes = 3;
+let TotalPerHourKilometer = TrainCrossPlace / TakeTimes;
+console.log('Train pass every hour :' + TotalPerHourKilometer);
+// Train pass every hour :50
+
+// Math Number 8
+let buyChocklate = 120;
+let KeepEveryPacket = 9;
+let ChocklatePerPacket = buyChocklate / KeepEveryPacket;
+console.log(ChocklatePerPacket);
+let PerPacket = Math.floor(ChocklatePerPacket);
+console.log(PerPacket);
+
+
+let TotalChocklate = buyChocklate - KeepEveryPacket * PerPacket;
+console.log('The shopkeeper will have chocklate: ' + TotalChocklate);
+// The shopkeeper will have chocolate: 3
+
+// Math Number 7
+let UseMachine = 6;
+let ProductMaking = 200;
+let AddingMachine = 2;
+let ProductNotSale = 3;
+let MakingProduct = ProductMaking / UseMachine * AddingMachine;
+console.log(MakingProduct);
+let TotalMakingProduct = (Math.floor(MakingProduct));
+console.log(TotalMakingProduct);
+let AddingTwoProduct = ProductMaking + TotalMakingProduct;
+console.log('Adding Exta Two Prduct: ' + AddingTwoProduct);
+let IfNotSale = AddingTwoProduct - ProductNotSale;
+console.log('If three product not sale: ' + IfNotSale);
+
+// Adding Exta Two Prduct: 266
+// If three product not sale: 263
+
+
